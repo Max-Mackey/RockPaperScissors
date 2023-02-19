cf97b4dc63eda088d7377f8cd800c13f74a4475e
Max-Mackey <maxmackey@gmail.com>
2023-02-19 11:04:01 -0800

    Final changes made, game now works successfully for a single round.

--- game.js
+++ game.js
@@ -6,42 +6,28 @@ Assign that choice to playerChoice
 */
 let playerChoice = prompt("Type you choice of Rock, Paper, or Scissors.")
 playerChoice = playerChoice.toUpperCase()
-console.log(playerChoice)
-
 /*
 Needs to create validation check that player chose 1 of the 3 options.This would be a for loop? 
 cpuChoice starting value = null
 Roll a random number, or pick a random value from a string of text?
 */
 
+/*Creates an array for var to populate from based on random number
+*/
 var words = ["ROCK", "PAPER", "SCISSORS"];
 var cpuChoice = words[Math.floor(Math.random() * words.length)];
-console.log(cpuChoice); 
     if (playerChoice === cpuChoice)
     {console.log("Player chose " +playerChoice+" and CPU chose "+cpuChoice+". Draw!");}
-    else {console.log("IDK")}
-
-
-/*
-Compare values
-Create variable for gameResult
-
-if cpuChoice == playerChoice gameResult = tie
-
-if playerChoice = Rock and cpuChoice == Paper, gameResult = lose
-if playerChoice = Rock and cpuChoice == Scissors, gameResult = win
-
-if playerChoice = Scissors and cpuChoice = Rock, gameResult = lose
-if playerChoice = Scissors and cpuChoice == Paper, gameResult = win
-
-if playerChoice = Paper and cpuChoice = Rock, gameResult = win
-if playerChoice = Paper and cpuChoice == Scissors, gameResult = lose
-
-declaration = null
-if gameResult = Win, declaration = beats
-if gameResult = Lose, declaration = loses to
-if gameResult = Tie, declaration = ties with
-
-print("You " & gameResult & playerChoice & declaration & cpuChoice)
-*/
+        else if (
+            (cpuChoice == "ROCK" && playerChoice == "SCISSORS") ||
+            (cpuChoice == "SCISSORS" && playerChoice == "PAPER")||
+            (cpuChoice == "PAPER" && playerChoice == "ROCK")
+        ) 
+        {console.log("Player chose " +playerChoice+" and CPU chose "+cpuChoice+". YOU LOSE :(");}
+            else if (
+                (playerChoice == "ROCK" && cpuChoice == "SCISSORS") ||
+                (playerChoice == "SCISSORS" && cpuChoice == "PAPER")||
+                (playerChoice == "PAPER" && cpuChoice == "ROCK")
+            )
+            {console.log("Player chose " +playerChoice+" and CPU chose "+cpuChoice+". !!!!YOU WIN!!!!");}
 
