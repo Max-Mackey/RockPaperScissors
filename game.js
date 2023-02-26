a3ce151cf3a1d73d2f0c48143b66b09c42bd26b7
Max-Mackey <maxmackey@gmail.com>
2023-02-25 16:03:48 -0800

    Trashed original code. Re-worked as functions.

--- game.js
+++ game.js
@@ -1,33 +1,17 @@
-/* 
-playerChoice starting value = null (must be string)
-Create a text prompt asking for playerChoice
-Make the player hoice all UpperCase()
-Assign that choice to playerChoice
-*/
-let playerChoice = prompt("Type you choice of Rock, Paper, or Scissors.")
-playerChoice = playerChoice.toUpperCase()
-/*
-Needs to create validation check that player chose 1 of the 3 options.This would be a for loop? 
-cpuChoice starting value = null
-Roll a random number, or pick a random value from a string of text?
-*/
+function getComputerChoice(cpuChoice){
+  const cpuNum = Math.floor(Math.random() * 3);
+  switch (cpuNum) {
+    case 0:
+      return 'rock';
+      case 1:
+        return 'paper';
+        case 2:
+          return 'scissors';
+  }
+}
+console.log(getComputerChoice())
 
-/*Creates an array for var to populate from based on random number
-*/
-var words = ["ROCK", "PAPER", "SCISSORS"];
-var cpuChoice = words[Math.floor(Math.random() * words.length)];
-    if (playerChoice === cpuChoice)
-    {console.log("Player chose " +playerChoice+" and CPU chose "+cpuChoice+". Draw!");}
-        else if (
-            (cpuChoice == "ROCK" && playerChoice == "SCISSORS") ||
-            (cpuChoice == "SCISSORS" && playerChoice == "PAPER")||
-            (cpuChoice == "PAPER" && playerChoice == "ROCK")
-        ) 
-        {console.log("Player chose " +playerChoice+" and CPU chose "+cpuChoice+". YOU LOSE :(");}
-            else if (
-                (playerChoice == "ROCK" && cpuChoice == "SCISSORS") ||
-                (playerChoice == "SCISSORS" && cpuChoice == "PAPER")||
-                (playerChoice == "PAPER" && cpuChoice == "ROCK")
-            )
-            {console.log("Player chose " +playerChoice+" and CPU chose "+cpuChoice+". !!!!YOU WIN!!!!");}
+const playerSelection = prompt("Please choose Rock, Paper, or scissors.").toLowerCase();
+console.log(playerSelection)
 
+function RockPaperScissors(playerSelection, getComputerChoice)
