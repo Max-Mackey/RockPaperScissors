0841c10be60e4115400f8404b87d5ff684ce5c8e
Max-Mackey <maxmackey@gmail.com>
2023-03-01 15:41:07 -0800

    fixed some RPS functionality before expanding in new lessons

--- game.js
+++ game.js
@@ -1,20 +1,17 @@
 // function that creates choice using math.random
-function getComputerChoice(){
+function getComputerChoice() {
   const cpuNum = Math.floor(Math.random() * 3);
   switch (cpuNum) {
     case 0:
       return 'rock';
-      case 1:
-        return 'paper';
-        case 2:
-          return 'scissors';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
   }
 }
 
-const playerSelection = prompt("Please choose Rock, Paper, or scissors.").toLowerCase();
-const computerSelection = getComputerChoice();
-
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection) {
   let result;
   if (playerSelection === computerSelection) {
     result = (`Tie game, you both chose ${playerSelection}.`);
@@ -29,16 +26,19 @@ function playRound(playerSelection, computerSelection){
   }
   return result;
 }
-console.log(playRound(playerSelection, computerSelection));
 
 function game() {
   let matchCount = 0;
-  let result;
 
-  for (let i = 0; matchCount < 5; i++) {
-    result = playRound(playerSelection, computerSelection);
+  while (matchCount < 5) {
+    const playerSelection = prompt("Please choose Rock, Paper, or scissors.").toLowerCase();
+    const computerSelection = getComputerChoice();
+    
+    const result = playRound(playerSelection, computerSelection);
     console.log(result);
+    
     matchCount++;
   }
 }
+
 game();
