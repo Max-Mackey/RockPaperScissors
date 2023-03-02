2cd334a551b7741408e522b2b87c3d9947a7e0a2
Max-Mackey <maxmackey@gmail.com>
2023-03-02 15:50:54 -0800

    Icons now work creating game alerts with results. Added HTML to display scores. Working on how to get scores to display

--- game.js
+++ game.js
@@ -1,21 +1,4 @@
-// function that creates choice using math.random
-const playerRock = document.querySelector('div.rock');
-playerRock.addEventListener('click', () => {
-  const playerChoice = "rock";
-  console.log(playerChoice);
-})
-const playerPaper = document.querySelector('div.paper');
-playerPaper.addEventListener('click', () => {
-  const playerChoice = "paper";
-  console.log(playerChoice);
-})
-const playerScissors = document.querySelector('div.scissors');
-playerScissors.addEventListener('click', () => {
-  const playerChoice = "scissors";
-  console.log(playerChoice);
-})
-
-function getComputerChoice() {
+function getComputerSelection() {
   const cpuNum = Math.floor(Math.random() * 3);
   switch (cpuNum) {
     case 0:
@@ -31,14 +14,38 @@ function playRound(playerSelection, computerSelection) {
   let result;
   if (playerSelection === computerSelection) {
     result = (`Tie game, you both chose ${playerSelection}.`);
+    alert(result);
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "scissors" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "rock")
   ) {
     result = (`You win! You chose ${playerSelection} and the computer chose ${computerSelection}.`);
+    alert(result);
   } else {
     result = (`You lose! You chose ${playerSelection} and the computer chose ${computerSelection}.`);
+    alert(result);
   }
   return result;
 }
+const playerRock = document.querySelector('div.rock');
+playerRock.addEventListener('click', () => {
+  const playerSelection = "rock";
+  const computerSelection = getComputerSelection();
+  const result = playRound(playerSelection, computerSelection);
+  console.log(result);
+})
+const playerPaper = document.querySelector('div.paper');
+playerPaper.addEventListener('click', () => {
+  const playerSelection = "paper";
+  const computerSelection = getComputerSelection();
+  const result = playRound(playerSelection, computerSelection);
+  console.log(result);
+})
+const playerScissors = document.querySelector('div.scissors');
+playerScissors.addEventListener('click', () => {
+  const playerSelection = "scissors";
+  const computerSelection = getComputerSelection();
+  const result = playRound(playerSelection, computerSelection);
+  console.log(result);
+})
