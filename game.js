7e96c1f59fbfa4a9137ec2aef21c9da12582fe16
Max-Mackey <maxmackey@gmail.com>
2023-03-03 05:06:09 -0800

    Project finished

--- game.js
+++ game.js
@@ -10,6 +10,9 @@ function getComputerSelection() {
   }
 }
 
+let i = 0;
+let x = 0;
+
 function playRound(playerSelection, computerSelection) {
   let result;
   if (playerSelection === computerSelection) {
@@ -21,13 +24,32 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === "paper" && computerSelection === "rock")
   ) {
     result = (`You win! You chose ${playerSelection} and the computer chose ${computerSelection}.`);
+    i = i+1;
     alert(result);
+    document.querySelector('div.playerScore').textContent = `${i}`;
+    
   } else {
     result = (`You lose! You chose ${playerSelection} and the computer chose ${computerSelection}.`);
+    x = x+1;
     alert(result);
+    document.querySelector('div.computerScore').textContent = `${x}`;
   }
+
+  //check if either player or computer has reached a score of 5
+  if (i === 5 || x === 5) {
+    let winner = i === 5 ? "Player" : "The Computer";
+    alert(`${winner} wins!`);
+
+    i = 0;
+    x = 0;
+    document.querySelector('div.playerScore').textContent = '0';
+    document.querySelector('div.computerScore').textContent = '0';
+  }
+
+
   return result;
 }
+
 const playerRock = document.querySelector('div.rock');
 playerRock.addEventListener('click', () => {
   const playerSelection = "rock";
@@ -49,3 +71,14 @@ playerScissors.addEventListener('click', () => {
   const result = playRound(playerSelection, computerSelection);
   console.log(result);
 })
+
+const myButton = document.querySelector('.reset');
+const myDiv = document.querySelector('.playerScore');
+const myDiv2 = document.querySelector('.computerScore');
+
+myButton.addEventListener('click', () => {
+  i = 0;
+  x = 0;
+  myDiv.textContent = '0';
+  myDiv2.textContent = '0';
+});
